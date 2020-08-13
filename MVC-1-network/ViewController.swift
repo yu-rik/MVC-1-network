e395c613424ff46c7bbe19dcee2024e8fcba3f2a
Yurii Nosachenko <yurik_n@outlook.com>
2020-08-13 22:34:31 +0300

    Initial Commit

---
+++ MVC-1-network/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC-1-network
+//
+//  Created by yurik on 8/13/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
